e4d7185280a0c4f179cc7731c954a64e236933e7
Sydney Petrehn <petr8004@vandals.uidaho.edu>
2020-12-01 16:36:50 -0800

    Creating Landing Page
    Initial Creation of Landing Page from following a tutorial online.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Body= ()=>{
+  return(
+    <div id="body">
+      <Header/>
+      <Card/>
+      <ContactContainer/>
+    </div>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Header = () =>{
+  return(
+    <div className='header'>
+      <span className='header-title'>
+        University of Idaho
+      </span>
+      <br/>
+      <span className="header-text">
+        Machine Scheduling Software for the Department of Engineering
+      </span>
+    </div>
+  );
+}
+
+const Card = (props) =>{
+  return(
+    <div className={props.className} >
+      <div className="small-div">
+        <i className={props.className}></i>
+        <img src={props.img} alt=''/>
+      </div>
+
+      <div className="big-div">
+        <span className='div-title'>
+          {props.title}
+        </span>
+        <br/>
+        <span>
+          {props.description}
+        </span>
+      </div>
+    </div>
+  )
+}
+
+// HERE IS WHERE THE PREVIEW OF THE SCHEDULING CALANDER GOES
+const Body = () => {
+  return(
+    <div id='body'>
+      <Header/>
+      <Card 
+          className='section'
+          title='Schedule Overview'
+          description='Weekly Schedule of Machine Software. Please login to create a reservation.'
+      />
+    </div>
+  )
+}
+
+ReactDOM.render(<Body/>, document.getElementById('root'));
