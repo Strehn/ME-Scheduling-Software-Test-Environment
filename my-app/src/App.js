f0a08a6f3303c7ce31cfa29b0e86b62320ad27f8
Sydney Petrehn <petr8004@vandals.uidaho.edu>
2020-12-01 19:59:14 -0800

    Creating Authentication

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,34 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+
+import { NavBar, Footer, Loading } from "./components";
+import { Home, Profile, ExternalApi } from "./views";
 import "./styles.css";
 
+const App = () => {
+    const { isLoading } = useAuth0();
+  
+    if (isLoading) {
+      return <Loading />;
+    }
+  
+    return (
+        <div id="app" className="d-flex flex-column h-100">
+          <NavBar />
+          <div className="container flex-grow-1">
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <ProtectedRoute path="/profile" component={Profile} />
+              <ProtectedRoute path="/external-api" component={ExternalApi} />
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      );
+    };
+  export default App;
+/*
 export default function App() {
   return(
         <div id='body'>
@@ -8,39 +36,10 @@ export default function App() {
             <Card 
                 className='section'
                 img='./Capture1.PNG'
-                title='About the Company' 
-                description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur.'
+                title='Overview of the Weekly Schedule' 
+                description='Here is a weekly schedule for the ME Machines, please login to make a reservation.'
             />
-
-            <Card 
-                className='section bg-grey'
-                img='./Capture3.PNG' 
-                title='Our Values'
-                description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur.'
-            />
-
-            <Card 
-                className='section'
-                img='./Capture1.PNG' 
-                title='Our Mission' 
-                description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur.'
-            />
-            <ContactContainer/>
+            <LoginContainer/>
         </div>
     );
 }
@@ -49,21 +48,15 @@ const Header = () =>{
     return(
         <div className='header'>
             <span className='header-title'>
-                Company
+                University of Idaho
             </span>
             <br/>
             <span className="header-text">
-                We specialise in something ...
+                Department of Engineering
             </span>
         </div>
     );
 }
-
-
-
-
-
-
 
 const Card = (props) =>{
     return(
@@ -86,40 +79,44 @@ const Card = (props) =>{
     )
 }
 
-
-
-const ContactContainer = () => {
+const LoginContainer = () => {
     return(
-        <div className='contact-container bg-grey'>
-            <span className="div-title">Contact us</span>
-            <div className='contact-form'>
+        <div className='login-container bg-grey'>
+            <span className="div-title">Login</span>
+            <div className='login-form'>
                 <div id='sect1'>
-                    <span>Contact us and we will get back to you within 24 hours.</span>
-                    <span>
-                        <i className="fas fa-map-marker-alt"></i>
-                        Kampala Uganda
+                     <span>
+                        Create an Account
                     </span>
                     <span>
-                        <i className="fas fa-mobile-alt"></i>
-                        +256 778 800 900
+                        Email Address: must be @vandals.uidaho.edu or @uidaho.edu
                     </span>
                     <span>
-                        <i className="far fa-envelope"></i>
-                        company.gmail.com
+                    <input type="text" placeholder="email address" className="input-field"/>
                     </span>
+                    <span>
+                        Email Address: must be @vandals.uidaho.edu or @uidaho.edu
+                    </span>
+                    <span>
+                        <input type="text" placeholder="password" className="input-field"/>
+                         <button className="contact-btn">CreateAccount</button>
+                    </span>
+                    
                 </div>
                     
                 <div id='sect2'>
                     <span>
-                        Contact
+                        Login
                     </span>
 
                     <input type="text" placeholder="email address" className="input-field"/>
-                    <textarea name="" id="" cols="30" rows="10" placeholder="comment"></textarea>
-                    <button className="contact-btn">Send</button>
+                    <input type="text" placeholder="password" className="input-field"/>
+                    <button className="contact-btn">Login</button>
                 </div>
             </div>
         </div>
     );
 
 }
+
+*/
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
-
+import "./styles.css";
+// for authentication using Auth0
+import { BrowserRouter as Router } from "react-router-dom";
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Router>
+    <Auth0ProviderWithHistory>
+      <App />
+    </Auth0ProviderWithHistory>
+  </Router>,
+  document.getElementById("root")
+);
